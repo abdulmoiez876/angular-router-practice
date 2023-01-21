d888366d9516de12dc19da75e61ff7e40b5124b4
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-01-21 12:19:17 +0500

    fetching the route params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -11,9 +12,13 @@ export class UserComponent implements OnInit {
     name: ''
   };
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.activatedRoute.snapshot.params['id'],
+      name: this.activatedRoute.snapshot.params['name']
+    }
   }
 
 }
